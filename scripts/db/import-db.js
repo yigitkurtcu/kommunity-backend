189bf25af01836b07b37b5ba0b2e6892cfc644c5
Mustafa Onur KILINÇ <mustafa.onur.kilinc@gmail.com>
2018-10-18 17:24:27 +0200

    fixed lint errors

--- scripts/db/import-db.js
+++ scripts/db/import-db.js
@@ -1,16 +1,16 @@
 const { exec } = require('child_process');
+
 const dbVer = 'v0.0';
-const sqlScriptFileName = 'kommunity.app.db.' + dbVer + '.sql';
-exec('mysql -u root -p < scripts\\db\\' + dbVer + '\\' + sqlScriptFileName, (err, stdout, stderr) => {
-    var resultMessage;
-    if (err) {
-        resultMessage = 'Import failed. (' + sqlScriptFileName + ')\n\n!!!!! => ' + err.message + '\n\n';
-    } else if(stderr) {
-        resultMessage = 'Import completed. (' + sqlScriptFileName + ')\n\n' + stderr + '\n\n';
-    } else if(stdout) {
-        resultMessage= 'Import completed. (' + sqlScriptFileName + ')\n\n' + stdout + '\n\n';
-    }
-    /* eslint-disable no-console */
-    console.log(resultMessage)
-    /* eslint-enable no-console */
+const sqlScriptFileName = `kommunity.app.db.${dbVer}.sql`;
+
+exec(`mysql -u root -p < scripts\\db\\${dbVer}\\${sqlScriptFileName}`, (err, stdout, stderr) => {
+  let resultMessage;
+  if (err) {
+    resultMessage = `Import failed. (${sqlScriptFileName})\n\n!!!!! => ${err.message}\n\n`;
+  } else if (stderr || stdout) {
+    resultMessage = `Import completed. (${sqlScriptFileName})\n\n${stderr || stdout}\n\n`;
+  }
+  /* eslint-disable no-console */
+  console.log(resultMessage);
+  /* eslint-enable no-console */
 });
