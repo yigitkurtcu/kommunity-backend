0eb74e7f99fd7ae42318bd93aff0b39643f694f6
Mustafa Onur KILINÇ <mustafa.onur.kilinc@gmail.com>
2018-10-22 17:24:37 +0200

    issue_22 review 1 update

--- scripts/db/import-db.js
+++ scripts/db/import-db.js
@@ -1,10 +1,23 @@
 const { exec } = require('child_process');
 
-const dbVer = 'v0.0';
-const sqlScriptFileName = `kommunity.app.db.${dbVer}.sql`;
+const dbVers = [
+    'v0.0' // First version.
+    // , 'v0.1'
+    // , 'v0.2'
+    // etc.
+  ];
+let dbVer = dbVers.slice(-1)[0];
+const verArg = process.argv.slice(2)[0];
 
+if (verArg) {
+  if (dbVers.indexOf(verArg) === -1) {
+    throw new Error(`Invalid db schema version. (${verArg})`);
+  } else {
+    dbVer = verArg;
+  }
+}
+const sqlScriptFileName = `kommunity.app.db.${dbVer}.sql`;
 exec(`mysql -u root -p < scripts\\db\\${dbVer}\\${sqlScriptFileName}`, (err, stdout, stderr) => {
-  let resultMessage;
   if (err) {
     resultMessage = `Import failed. (${sqlScriptFileName})\n\n!!!!! => ${err.message}\n\n`;
   } else if (stderr || stdout) {
