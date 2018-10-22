3be1edddd3144ed2fbbe2aca12b62b7d44e253f5
selmankahya <selmankahya@hotmail.com>
2018-10-21 21:05:12 -0700

    fixing test failure

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import { initializeExpressApp } from './app-init';
 import { initializePassport } from './passport/init';
 import { initializeRoutes } from './routes';
-import { initializeApolloServer } from './graphql/server';
 
 // initialize app
 const app = initializeExpressApp();
 initializePassport(app);
 initializeRoutes(app);
-initializeApolloServer(app);
 
 module.exports = app;
