bf590254bfcfff702aab2b1794fd3480c681c2c0
Selman Kahya <selmankahya@hotmail.com>
2018-10-22 09:40:35 -0700

    Update appSettings.js

--- flow-typed/appSettings.js
+++ flow-typed/appSettings.js
@@ -1,17 +1,14 @@
 export class AppServerSettings {
   hostname: string;
-
   port: number;
 }
 
 export class DbServerSettings {
   hostname: string;
-
   port: number;
 }
 
 export class AppSettings {
   appServer: AppServerSettings = new AppServerSettings();
-
   dbServer: DbServerSettings = new DbServerSettings();
 }
